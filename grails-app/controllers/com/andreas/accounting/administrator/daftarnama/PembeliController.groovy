429ab197dd1e27c58800a4b6e529d695af8fa5d6
Andreas Dharmawan <andreas@divusi.com>
2016-02-14 13:22:46 +0700

    pembeli modul

---
+++ grails-app/controllers/com/andreas/accounting/administrator/daftarnama/PembeliController.groovy
@@ -0,0 +1,44 @@
+package com.andreas.accounting.administrator.daftarnama
+
+import grails.converters.JSON
+
+class PembeliController {
+    
+    def pembeliService
+
+    def index() {
+        render pembeliService.listAll() as JSON
+    }
+    
+    def list() {
+        params.offset = params.offset != 'null' ? params.int('offset') : 0
+        params.max = params.max != 'null' ? params.int('max') : 10
+        params.sort = params.sort != 'null' ? params.sort : 'nama'
+        params.order = params.order != 'null' ? params.order : 'asc'
+        render pembeliService.list(params) as JSON
+    }
+    
+    def listNama() {
+        render pembeliService.listNama() as JSON
+    }
+    
+    def save() {
+        render pembeliService.save(request.JSON) as JSON
+    }
+    
+    def get(long id) {
+        render pembeliService.get(id) as JSON
+    }
+    
+    def update(long id) {
+        render pembeliService.update(id, request.JSON) as JSON
+    }
+    
+    def delete(long id) {
+        render pembeliService.delete(id) as JSON
+    }
+    
+    def count() {
+        render pembeliService.count(params)
+    }
+}
---
+++ grails-app/services/com/andreas/accounting/administrator/daftarnama/PembeliService.groovy
@@ -0,0 +1,145 @@
+package com.andreas.accounting.administrator.daftarnama
+
+import com.andreas.accounting.administrator.daftarnama.Orang
+import com.andreas.accounting.administrator.daftarnama.Perusahaan
+import grails.converters.JSON
+import grails.transaction.Transactional
+import org.hibernate.criterion.CriteriaSpecification
+
+@Transactional
+class PembeliService {
+    
+    def listAll() {
+        return Orang.withCriteria {
+            resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
+            perusahaan {
+                eq('activeStatus', 'Y')
+            }
+            eq('tipe', 'CUSTOMER')
+            eq('activeStatus', 'Y')
+            projections {
+                property('id', 'id')
+                property('nama', 'nama')
+                property('telepon', 'telepon')
+                property('hp', 'hp')
+                property('perusahaan', 'perusahaan')
+            }
+        }
+    }
+    
+    def list(params) {
+        return Orang.withCriteria {
+            resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
+            perusahaan {
+                eq('activeStatus', 'Y')
+            }
+            eq('tipe', 'CUSTOMER')
+            eq('activeStatus', 'Y')
+            order(params.sort, params.order)
+            maxResults(params.max)
+            firstResult(params.offset)
+            projections {
+                property('id', 'id')
+                property('nama', 'nama')
+                property('telepon', 'telepon')
+                property('hp', 'hp')
+                property('perusahaan', 'perusahaan')
+            }
+        }
+    }
+    
+    def listNama() {
+        return Orang.withCriteria {
+            resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
+            perusahaan {
+                eq('activeStatus', 'Y')
+            }
+            eq('tipe', 'CUSTOMER')
+            eq('activeStatus', 'Y')
+            projections {
+                property('id', 'id')
+                property('nama', 'nama')
+            }
+        }
+    }
+
+    def save(data) {
+        def orang = new Orang()
+        orang.tipe = 'CUSTOMER'
+        orang.nama = data.nama
+        orang.telepon = data.telepon
+        orang.hp = data.hp
+        orang.activeStatus = 'Y'
+        orang.perusahaan = Perusahaan.get(data.perusahaan.id)
+        
+        def response = [:]
+        if (orang.save(flush: true)) {
+            response['message'] = 'succeed'
+            response['id'] = orang.id
+        } else {
+            response['message'] = 'failed'
+        }
+        return response
+    }
+    
+    def get(id) {
+        return Orang.withCriteria {
+            resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
+            perusahaan {
+                eq('activeStatus', 'Y')
+            }
+            eq('id', id)
+            eq('tipe', 'CUSTOMER')
+            eq('activeStatus', 'Y')
+            projections {
+                property('id', 'id')
+                property('nama', 'nama')
+                property('telepon', 'telepon')
+                property('hp', 'hp')
+                property('perusahaan', 'perusahaan')
+            }
+        }[0]
+    }
+    
+    def update(id, data) {
+        def orang = Orang.get(id)
+        orang.tipe = 'CUSTOMER'
+        orang.nama = data.nama
+        orang.telepon = data.telepon
+        orang.hp = data.hp
+        orang.perusahaan = Perusahaan.get(data.perusahaan.id)
+        
+        def response = [:]
+        if (orang.save(flush: true)) {
+            response['message'] = 'succeed'
+            response['id'] = orang.id
+        } else {
+            response['message'] = 'failed'
+        }
+        return response
+    }
+    
+    def delete(id) {
+        def orang = Orang.get(id)
+        orang.activeStatus = 'N'
+        
+        def response = [:]
+        if (orang.save(flush: true)) {
+            response['message'] = 'succeed'
+        } else {
+            response['message'] = 'failed'
+        }
+        return response
+    }
+    
+    def count(params) {
+        return Orang.withCriteria {
+            resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
+            perusahaan {
+                eq('activeStatus', 'Y')
+            }
+            eq('tipe', 'CUSTOMER')
+            eq('activeStatus', 'Y')
+        }.size()
+    }
+}
